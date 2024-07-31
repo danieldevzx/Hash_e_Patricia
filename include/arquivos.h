affa8945951c80dd51aea350d4d7deafdc5ac5e8
Daniel <danielmartinsdev@gmail.com>
2024-07-31 20:27:14 -0300

    Inicio

---
+++ include/arquivos.h
@@ -0,0 +1,13 @@
+#ifndef ARQUIVOS_H
+#define ARQUIVOS_H
+
+#include <stdio.h>
+
+typedef struct {
+    FILE *arquivo;
+} Arquivo;
+
+int abreArquivo(char *nome, Arquivo *arquivo);
+int
+
+#endif
---
+++ include/palavras_TAD.h
@@ -0,0 +1,7 @@
+#ifndef PALAVRAS_TAD_H
+#define PALAVRAS_TAD_H
+
+#include <stdio.h>
+
+
+#endif
