125d49eb8c17ca281d006d77cfa34ef64cbb3621
GofArzy <145512691+GOFArzy@users.noreply.github.com>
2024-08-05 08:11:20 -0300

    Update patricia_TAD.h

--- include/patricia_TAD.h
+++ include/patricia_TAD.h
@@ -8,35 +8,50 @@
 -Ordenador de índices
 -Pesquisa
 */
-
 #ifndef PATRICIA_TAD_H_INCLUDED
 #define PATRICIA_TAD_H_INCLUDED
 
+#include "palavras_TAD.h"
+
+typedef struct PatriciaNode PatriciaNode;
+typedef struct {
+    PatriciaNode *root;
+} PatriciaTree;
+
+PatriciaTree* createPatricia();
+void insertPatricia(PatriciaTree *tree, const char *key, tIndiceInvertido indice);
+PatriciaNode* searchPatricia(PatriciaTree *tree, const char *key);
+void printPatriciaNode(PatriciaNode *node);
+void printPatricia(PatriciaTree *tree);
+void liberaPatricia(PatriciaTree *tree);
+char* palavraParaBinario(const char *palavra);
+
 typedef struct tInvertedIndex *pInvertedIndexes;
-typedef struct tInvertedIndex{
+typedef struct tInvertedIndex {
     int qtnd, idDoc;
     int len;
-}tInvertedIndex;
+} tInvertedIndex;
 
 typedef int typeIndex;
 typedef int typeBit;
-typedef enum{
+typedef enum {
     Inside, Outside
 } typeOfNode;
 
 typedef struct tNode *pNode;
-typedef struct tNode{
+typedef struct tNode {
     typeOfNode type;
     union {
-        struct 
-        {
+        struct {
             typeIndex index;
             pNode lef, rig;
-        }insideNode;
-        char *key;
-        pInvertedIndexes indexesList;
-    }Node;
-}tNode;
+        } insideNode;
+        struct {
+            char *key;
+            pInvertedIndexes indexesList;
+        } outsideNode;
+    } Node;
+} tNode;
 
 void fPrintKey(char *key, int len);
 void fConvertToBin(char *key, char *bin, int size);
